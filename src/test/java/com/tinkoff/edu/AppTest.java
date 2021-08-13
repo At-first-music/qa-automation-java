c1e8b136cfd05aeea67ca7c8091545750d1c9253
rbah6 <rburnashevost@gmail.com>
2021-08-13 13:47:15 +0300

    rename for old name

--- src/test/java/com/tinkoff/edu/AppTest.java
+++ src/test/java/com/tinkoff/edu/AppTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
  * Unit test for simple App.
  */
-public class CreditCalcRepositoryTest {
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
