da539285f995973b9d1389f7ce35d590012f8909
rbah6 <rburnashevost@gmail.com>
2021-08-25 14:57:08 +0300

    Practice Quest: add test

--- src/test/java/com/tinkoff/edu/AppTest.java
+++ src/test/java/com/tinkoff/edu/AppTest.java
@@ -1,18 +1,39 @@
 package com.tinkoff.edu;
 
+import com.tinkoff.edu.app.controller.CreditCalcController;
+import com.tinkoff.edu.app.models.CreditRequest;
+import com.tinkoff.edu.app.models.CreditResponse;
+import com.tinkoff.edu.app.repository.DefaultCreditCalcRepository;
+import com.tinkoff.edu.app.service.DefaultCreditCalcService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static com.tinkoff.edu.app.enums.ClientType.*;
+import static com.tinkoff.edu.app.enums.ResponseType.CONFIRM_REQUEST;
+import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
+    private CreditCalcController creditCalcController;
+    private CreditRequest creditRequest;
+
+    @BeforeEach
+    public void init() {
+        // Given
+        creditCalcController = new CreditCalcController(new DefaultCreditCalcService(new DefaultCreditCalcRepository()));
+    }
+
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void shouldCorrectRequest() {
+        // Given
+        creditRequest = new CreditRequest(IP, 10, 100);
+
+        //When
+
+        //Then
+        assertEquals("CreditResponse {" +
+              "requestId=" + 1 +
+              ", responseType=" + CONFIRM_REQUEST +
+              ", CreditRequest=" + creditRequest +
+              " }", String.valueOf(creditCalcController.createRequest(creditRequest)));
     }
 }
